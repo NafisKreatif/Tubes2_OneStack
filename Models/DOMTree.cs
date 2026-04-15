325c3dd4b46eeef23a6e14a06f5e188eb99f3238
wzlyy <wildanag.10@gmail.com>
2026-04-15 09:35:26 +0700

    feat: DOMTree class

---
+++ Models/DOMTree.cs
@@ -0,0 +1,47 @@
+namespace DOMTreeTraversal.Models;
+using System;
+using System.Collections.Generic;
+
+//2 Jenis DOMNode: Tag atau Text
+public class DOMNode 
+{
+    
+    // tag (<body>, </div>, </span>, dll) 
+    public string Tag { get; set; }
+    // teks
+    // contoh : "<body>Hello</body>, Hello berarti Text"
+    public string Text { get; set; }
+    public List<DOMNode> Children { get; set; }
+    public DOMNode Parent { get; set; }
+    //constructor
+    public DOMNode(string tag = "", string text = "")
+    {
+        Tag = tag;
+        Text = text;
+        Children = new List<DOMNode>();
+    }
+    
+    public bool IsTextNode()
+    {
+        return string.IsNullOrEmpty(Tag) && !string.IsNullOrEmpty(Text);
+    }
+}
+public class DOMTree
+{
+    public DOMNode Root { get; set; }
+
+    //constructor
+    public DOMTree()
+    {
+        Root = new DOMNode("document");
+    }
+
+
+    public DOMNode AddChild(DOMNode parent, string tag, string text = "")
+    {
+        var node = new DOMNode(tag, text);
+        node.Parent = parent;
+        parent.Children.Add(node);
+        return node;
+    }
+}
