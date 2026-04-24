884efccbc18dc0528c4ebffade9360fbdb687bf5
Muhammad Nafis Habibi <137036100+NafisKreatif@users.noreply.github.com>
2026-04-24 13:21:06 +0700

    feat: add models for input and send data to view

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DOMTreeTraversal.Models;
+using DOMTreeTraversal.Test;
+using System.Text.Json;
 
 namespace DOMTreeTraversal.Controllers;
 
@@ -11,9 +13,38 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult About()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Result([Bind("link", "htmlText", "inputType", "traversalType")] InputModel model, IFormFile htmlFile)
+    {
+        Console.WriteLine(model.inputType);
+        string html = "";
+        switch (model.inputType)
+        {
+            case "link":
+                html = model.link ?? "";
+                break;
+            case "file":
+                if (htmlFile != null && htmlFile.Length > 0) html = htmlFile.FileName ?? "";
+                break;
+            case "text":
+                html = model.htmlText ?? "";
+                break;
+            default:
+                break;
+        }
+
+        JsonableDomTree test = new JsonableDomTree(DOMTreeStub.GetTestCase1());
+        for (int i = 0; i < test.Nodes.Count; i++)
+        {
+            Console.WriteLine(i + " : " + test.Nodes[i]);
+        Console.WriteLine(JsonSerializer.Serialize(test.Nodes));
+        }
+        return View(new JsonModel(JsonSerializer.Serialize(test.Nodes)));
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Models/InputModel.cs
@@ -0,0 +1,9 @@
+namespace DOMTreeTraversal.Models;
+
+public class InputModel
+{
+    public string? link;
+    public string? htmlText;
+    public string? inputType;
+    public string? traversalType;
+}
