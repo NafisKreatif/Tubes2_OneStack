5ef9fa17fa3814959525a11726a73a58470918f5
Jordan <ferimeison@gmail.com>
2026-04-25 18:07:36 +0700

    fix: maxSelected singular token

--- Models/CSSSelector.cs
+++ Models/CSSSelector.cs
@@ -29,6 +29,17 @@ public static class CSSSelector
         DateTime startTime = DateTime.Now;
 
         var currNodes = GetNodes(root, method, n => MatchSelector(n, tokens[0]));
+        if (tokens.Count == 0 && currNodes.Count > maxSelected)
+        {
+            var temp = new List<DOMNode>();
+            int i = 0;
+            foreach (var nodes in currNodes)
+            {
+                if (i < maxSelected) temp.Add(nodes);
+                else break;
+                i++;
+            }
+        }
         
         // Filterrrrrrrrr
         int lastToken = tokens.Count - 2;
